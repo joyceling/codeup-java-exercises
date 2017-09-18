3722df005bb3b68bc23460b1c27d3af68d78b07d
ironicsushi <yuli5565@colorado.edu>
2017-09-18 10:13:28 -0500

    *initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,10 @@
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.print("Hello Redwood!");
     }
+
 }
 
 
