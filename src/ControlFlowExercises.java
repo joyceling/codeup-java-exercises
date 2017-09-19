f419491e5e96928fc0b42ac941f139acb5862a94
ironicsushi <yuli5565@colorado.edu>
2017-09-19 14:14:49 -0500

    *loop basic - while loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // LOOP BASICS
+        // While loop
+            int i = 5;
+            while (i <= 15) {
+                System.out.println(i);
+                i++;
+            }
+    };
+}
