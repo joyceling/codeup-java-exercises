1d2af0b0ec929f5bc1a699f9fde76b267e13fabb
ironicsushi <yuli5565@colorado.edu>
2017-09-19 15:01:12 -0500

    *loop basic - dowhile loop that starts at 2 and squares itself while it's less than 100,000

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,15 +2,19 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         // LOOP BASICS
         // do-while loop
-        int i = 100;
+        long i = 2;
+        int base = 1;
+
         do {
-            if(i % 5 == 0) {
+            if (i <= 2) {
                 System.out.println(i);
-            }
-            i--;
-        } while (i >= -10);
+                i = i*i;
+            } else {
+                System.out.println(i);
+                i = (i * i);
+            };
 
-
+        } while (i < 100000);
 
     };
 }
