ca0fd9cbacf7a9214a6cd530bb6a20faa41be28c
ironicsushi <yuli5565@colorado.edu>
2017-09-19 15:17:07 -0500

    *loop basic - for loop that counts backwards by fives from 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,8 +3,8 @@ public class ControlFlowExercises {
         // LOOP BASICS
         // for loop
 
-        for (int i = 0; i <= 100; i++) {
-            if (i % 2 == 0) {
+        for (int i = 100; i >= -10; i--) {
+            if (i % 5 == 0) {
                 System.out.println(i);
             }
         }
