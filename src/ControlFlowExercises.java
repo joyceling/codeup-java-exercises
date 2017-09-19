542f16e066db132d95559197baf60d4bfff82634
ironicsushi <yuli5565@colorado.edu>
2017-09-19 14:35:53 -0500

    *loop basic - dowhile loop that counts backwards by fives from 100 to -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,13 +2,13 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         // LOOP BASICS
         // do-while loop
-        int i = 0;
+        int i = 100;
         do {
-            if(i % 2 == 0) {
+            if(i % 5 == 0) {
                 System.out.println(i);
             }
-            i++;
-        } while (i <= 100);
+            i--;
+        } while (i >= -10);
 
 
 
